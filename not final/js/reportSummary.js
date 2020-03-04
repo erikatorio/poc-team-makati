3c8a2f8b7e38c98d42206710d07024878b81b728
erikatorio <2160120@slu.edu.ph>
2020-03-04 17:05:48 +0800

    initial notifs change

--- not final/js/reportSummary.js
+++ not final/js/reportSummary.js
@@ -191,10 +191,12 @@ async function showNotif() {
 var isPaneOpen = false;
 function showNotifs() {
     if (!isPaneOpen) {
+        $('#chat-container').after('<div class="notifications-container"><div id="notif-toast" class="toast" role="alert" data-autohide="false" aria-live="assertive" aria-atomic="true"><div id="notif-header" class="toast-header"><img src="./img/flower.png" class="rounded mr-2" alt="..."><strong class="mr-auto">Notifications</strong></div><div id="indiv_notifs"></div></div></div>');
         $('#notif-toast').toast('show');
         isPaneOpen = true;
     } else {
         $('#notif-toast').toast('hide');
+        $('.notifications-container').remove()
         isPaneOpen = false;
     }
 }
